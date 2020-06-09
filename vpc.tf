825b2b7b0118eed3a6f6dca661f96e145bcecf71
Mohammed Wahaj <mohammedwahaj77@hotmail.com>
2020-06-09 20:13:24 +0000

    Added VPC module

--- vpc.tf
+++ vpc.tf
@@ -2,3 +2,12 @@ module "vpc" {
   source  = "app.terraform.io/mohammedwahaj-training/vpc/aws"
   version = "2.39.0"
 }
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "dedicated"
+
+  tags = {
+    Name = "main"
+  }
+}
